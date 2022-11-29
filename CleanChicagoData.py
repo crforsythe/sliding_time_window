f0f9811fb1088c82bb7063ae491b542626d7105a
burnsaar <74790577+burnsaar@users.noreply.github.com>
2022-11-29 18:47:00 -0500

    Update file path for clean chicago data

--- CleanChicagoData.py
+++ CleanChicagoData.py
@@ -1,7 +1,7 @@
 import pandas as p
 import numpy as np
 from tqdm import tqdm
-file = r"C:\Users\Connor\Documents\Chicago TNC Data\Transportation_Network_Providers_-_Trips.csv"
+file = r"G:\Other computers\Aaron's Laptop\Documents\GitHub\sliding_time_window\ChicagoTripTimeData.csv"
 usecols = ['Trip Start Timestamp', 'Trip End Timestamp', 'Trip Seconds']
 data = p.read_csv(file, usecols=usecols, chunksize=1000000)
 
